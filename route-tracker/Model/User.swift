3b9db6d866dcd788fab0c86ee006aa6a1e9e6afc
Frostfell <ichernyshov@bk.ru>
2018-09-24 19:45:52 +0300

    Added PrimaryKey for Login field

--- route-tracker/Model/User.swift
+++ route-tracker/Model/User.swift
@@ -14,6 +14,10 @@ class User: Object {
   @objc dynamic var login: String = ""
   @objc dynamic var password: String = ""
   
+  override static func primaryKey() -> String? {
+    return "login"
+  }
+  
   convenience init (login: String, password: String) {
     self.init()
     self.login = login
