189696c7a59ff9ab1abb7f8cd7b2e639b3c4a2c5
Frostfell <ichernyshov@bk.ru>
2018-09-24 21:16:40 +0300

    Revert "Added PrimaryKey for Login field"
    This reverts commit 3b9db6d866dcd788fab0c86ee006aa6a1e9e6afc.

--- route-tracker/UI/Modules/Auth/User.swift
+++ route-tracker/UI/Modules/Auth/User.swift
@@ -14,10 +14,6 @@ class User: Object {
   @objc dynamic var login: String = ""
   @objc dynamic var password: String = ""
   
-  override static func primaryKey() -> String? {
-    return "login"
-  }
-  
   convenience init (login: String, password: String) {
     self.init()
     self.login = login
