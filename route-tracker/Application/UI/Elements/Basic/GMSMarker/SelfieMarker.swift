d11ea17fa4a94885984c3ed8c09b60a8e66246e7
Frostfell <ichernyshov@bk.ru>
2018-10-11 16:56:50 +0300

    Added a custom selfie marker class. TODO: configure marker view to display a selfie in case it exists

---
+++ route-tracker/Application/UI/Elements/Basic/GMSMarker/SelfieMarker.swift
@@ -0,0 +1,19 @@
+//
+//  SelfieMarker.swift
+//  route-tracker
+//
+//  Created by Igor Chernyshov on 11/10/2018.
+//  Copyright © 2018 Igor Chernyshov. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+
+class SelfieMarker: GMSMarker {
+  
+  override init() {
+    super.init()
+    
+    // TODO: Configure marker view here
+  }
+}
